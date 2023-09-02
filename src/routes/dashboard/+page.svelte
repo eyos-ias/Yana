7f73120e15f1cba70f3cdb4e8dcc007a6b51a073
Brook Solomon <officalbrooksolo@gmail.com>
2023-09-02 09:12:46 +0300

    courses page done

--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -3,7 +3,7 @@
     import { collection, getDoc, doc, getDocs } from "firebase/firestore";
     import {onMount} from "svelte";
 
-    let listOfChapters = [];
+    let listOfCourses = [];
 
     async function getCoursesList(){
         //const users_ref = collection(db, 'users');
@@ -16,7 +16,7 @@
         // Extract the data from each document
 
 
-         listOfChapters = querySnapshot.docs.map((doc) => {
+         listOfCourses = querySnapshot.docs.map((doc) => {
             return doc.data().name;
         });
 
@@ -40,11 +40,47 @@
 
 </script>
 <section>
-    {#if (listOfChapters.length <= 0)}
-        Loading...
+    {#if (listOfCourses.length <= 0)}
+    <span class="loading loading-spinner loading-lg"></span>
     {:else}
-        {listOfChapters}
+    {#each listOfCourses as course}
+    <button class="btn"><a href="./course/{course}">    <div class="card w-96 bg-base-100 shadow-xl ">
+        <div class="card-body">
+            {#if course[1] == 's' || course[1] == 'S'}
+            <div class="badge badge-primary">Main couse</div>
+            {:else if course[1] == 'c' || course[1] == 'C'}
+            <div class="badge badge-accent">Common Course</div>
+            
+            {/if}
+
+          <h2 class="card-title"><img alt="course icon" src="open-book.png" width="40px">{course}</h2>
+          <p >course descripton</p>
+        </div>
+      </div>
+    </a>
+    </button>
+    {/each}
+        
     {/if}
 
 
 </section>
+
+<style>
+    .btn{
+        padding: 0px;
+        background: transparent;
+        border: 0px;
+    }
+    .card{
+        margin:20px;
+    }
+    .card:hover{
+        background-color: #f2f2f29f;
+        transition: 0.2s;
+    }
+    a{
+        text-decoration: none;
+        text-align: center;
+    }
+</style>
